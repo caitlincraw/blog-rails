924ee87553af5011bb7c4b94883bab51681f5804
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-04-02 11:09:29 -0400

    Add comments model and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
   # routes method that maps all of the conventional routes for a collection of resources
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
